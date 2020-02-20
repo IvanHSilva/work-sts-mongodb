eb671a70ae28745b522a11297e280f7ff2f2d219
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-02-20 19:57:34 -0300

    Project created

---
+++ src/main/java/br/com/hvc/workmongodb/WorkMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.hvc.workmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hvc/workmongodb/WorkMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hvc.workmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
