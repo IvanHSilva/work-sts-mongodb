8a603d66b93c24ce81ed8369f5d6f5eb799c8543
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-02-27 20:38:23 -0300

    Endpoint to return the posts of an user

--- src/main/java/br/com/hvc/workmongodb/resources/UserResource.java
+++ src/main/java/br/com/hvc/workmongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.hvc.workmongodb.domain.Post;
 import br.com.hvc.workmongodb.domain.User;
 import br.com.hvc.workmongodb.dto.UserDTO;
 import br.com.hvc.workmongodb.services.UserService;
@@ -61,5 +62,11 @@ public class UserResource {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
